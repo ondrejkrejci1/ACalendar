4080494de3c0dd0bd71a47f4a580fc7a6dbe76ad
Ondra <ondra.krejci13@gmail.com>
2025-05-30 11:33:28 +0200

    work

--- ACalendar/UI/Calendar.cs
+++ ACalendar/UI/Calendar.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ACalendar.UI
 {
     public class Calendar
     {
+        private List<DayTile> days;
+        private Grid calendar;
+
+        public List<DayTile> Days {  get { return days; } }
+
+        public Calendar(Grid grid)
+        {
+
+        }
+
+
+        public void GenerateMonthTiles(int year,int month)
+        {
+
+        }
+
+        public void PlaceDayTiles(int year,int month)
+        {
+
+        }
+
+        public DayTile GetTile(int index)
+        {
+            return Days[index];
+        }
+
+        public void UpdateCalendar(int year, int month)
+        {
+
+
+
+        }
+
+
+
     }
 }
